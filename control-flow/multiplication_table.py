435d5fcc3311f2e8fba9f2401984e641ce2d0279
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-06-30 13:43:18 +0200

    made changes

--- control-flow/multiplication_table.py
+++
@@ -1,3 +0,0 @@
-number =int(input("Enter a number for the multiplication table:"))
-for y in range(1,11):
-    print(number, "*", y,"=", number*y)
--- control-flow/python_introduction/rectangle_area.py
+++
@@ -1,4 +0,0 @@
-lenght = 10
-width = 5
-area = lenght * width
-print(f"The area of the rectangle is: {area}")
