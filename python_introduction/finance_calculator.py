3eef8b284777fc56baabc232cd869590acc682bd
se-thato <thatoselepe80@gmail.com>
2024-06-30 21:38:05 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_income = float(input("Enter your monthly income: "))
-total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = int(input("Enter your monthly income: "))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - total_monthly_expenses
 
 annual_interest_rate = 0.05
