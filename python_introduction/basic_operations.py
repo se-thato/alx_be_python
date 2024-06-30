9fed11b5531400005c520612892ee19e65c9cf8b
se-thato <thatoselepe80@gmail.com>
2024-06-30 14:08:08 +0200

    Update basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum_total = number1 + number2
+difference_total = number1 - number2
+product_total = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_total}")
+print(f"Subtraction of {number1} and {number2} is {difference_total}")
+print(f"Multiplication of {number1} and {number2} is {product_total}")
