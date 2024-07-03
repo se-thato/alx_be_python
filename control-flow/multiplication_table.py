5c56a80c4c596573512d3117ea101a0a07063279
se-thato <thatoselepe80@gmail.com>
2024-07-04 01:33:00 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
-number =input("Enter a number for the multiplication table: ")
+number =input("Enter a number to see its multiplication table: ")
 for i in range(1, 11):
     print(f"{number} * {i} = {number * i} ")
