60d2005168101a5eb1a3aa88bf9a6f5ace1dbb1e
se-thato <thatoselepe80@gmail.com>
2024-07-05 16:08:20 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,25 +11,25 @@ def make_reminder(task, priority, time_bound):
         case "high":
             Reminder = f"Reminder: '{task}' is a high priority task "
             if time_bound == "yes":
-                Reminder += "that requires immediate attention today!"
+                Reminder += "that requires immediate attention today! "
             else:
-                Reminder += "Consider completing it when you have free time."
+                Reminder += "Consider completing it when you have free time. "
             return Reminder
         
         case "medium":
             Reminder = f"Reminder: '{task}' is a medium priority task "
             if time_bound == "yes":
-                Reminder += "that requires immediate attention today!"
+                Reminder += "that requires immediate attention today! "
             else:
-                Reminder += "Consider completing it when you have free time."
+                Reminder += "Consider completing it when you have free time. "
             return Reminder
         
         case "low":
             Reminder = f"Note: '{task}' is a low priority task. "
             if time_bound == "yes":
-                Reminder += "that requires immediate attention today!"
+                Reminder += "that requires immediate attention today! "
             else:
-                Reminder += "Consider completing it when you have free time."
+                Reminder += "Consider completing it when you have free time. "
             return Reminder
     
 def main():
