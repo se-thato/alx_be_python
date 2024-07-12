806d7ff92cb9c97a33763398b639583a488339ac
se-thato <thatoselepe80@gmail.com>
2024-07-12 23:58:34 +0200

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,12 +2,12 @@ FAHRENHEIT_TO_CELCIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit): 
-    global FAHRENHEIT_TO_CELCIUS_FACTOR = 5/9
+    global FAHRENHEIT_TO_CELCIUS_FACTOR 
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELCIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+    global CELSIUS_TO_FAHRENHEIT_FACTOR 
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
