fe45dad80961473c29a7ebb905b426b87fe2af4b
se-thato <thatoselepe80@gmail.com>
2024-07-15 23:35:48 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,4 +23,4 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self._account_balance}")
+        return f"Current Balance: ${self._account_balance}"
