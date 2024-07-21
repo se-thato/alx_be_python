9cfd0bd02d4146f823354b49d5a87bc12ed534fb
thatoselepe80@gmail.com <thatoselepe80@gmail.com>
2024-07-21 17:29:29 +0200

    add file

---
+++ oop/class_static_method_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    #attribute
+    calculation_type = "Arithmetic Operation"
+
+    @staticmethod
+    def add(a, b):
+        return a + b 
+
+    @classmethod
+    def multiply(cls, a, b)
+    print(f"Calculation Type: {cls.calculation_type}")
+    return a * b 
---
+++ oop/main.py
@@ -0,0 +1,13 @@
+from class_static_method_demo import Calculator
+
+def main():
+    #use nstatic method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    #use classmethod
+    product_result = Calculator.muliply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
