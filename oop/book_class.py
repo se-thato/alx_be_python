9fc509437adc34a47acd26c84b8c922482817545
se-thato <thatoselepe80@gmail.com>
2024-07-28 02:50:11 +0200

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -10,7 +10,7 @@ class Book:
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Book ('{self.title}', '{self.author}', {self.year})"
     
 
